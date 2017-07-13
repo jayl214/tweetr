964b591f8e7312f7a76f777080f87cdbd7c110e5
jayl214 <jay.lin@mail.mcgill.ca>
2017-07-13 18:16:52 +0000

    mongodb implemented yay!

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,18 +9,33 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+
+
+      db.collection('tweets').insertOne(newTweet, function(err, result) {
+        if (err) {
+          return callback(err);
+        };
+
         callback(null, true);
       });
+
+
+
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
+
+
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        };
+        console.log(tweets);
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+        callback(null, tweets.sort(sortNewestFirst));
       });
+
     }
 
   };
